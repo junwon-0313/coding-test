0fd37c6a1c22a0ce587fcb6c2bc821994a66c6cb
junwon <jjuny9798@gmail.com>
2023-10-18 00:32:00 +0900

    solve: 루돌프의 반란

---
+++ 삼성 SW 역량 테스트 기출 문제/코드트리/루돌프의 반란.py
@@ -0,0 +1,145 @@
+def main():
+    santa_direction = [(-1,0),(0,1), (1,0),(0,-1)]
+    # 재귀 함수
+    def interaction(santa_num, dx,dy):
+        # 상호작용으로 밀려날 떄는 해당 좌표가 이미 값으로 채워짐
+        x,y = santa_lst[santa_num]
+        nx, ny = x+dx, y+dy
+        #범위를 넘어가면
+        if nx<1 or nx>N or ny<1 or ny>N:
+            out_lst.append(santa_num)
+            santa_lst[santa_num]=(-1,-1)
+            return
+        if graph[nx][ny]!=0:
+            interaction(graph[nx][ny],dx,dy)
+        santa_lst[santa_num] = [nx,ny]
+        graph[nx][ny]=santa_num
+
+    def crash_rudolph(x,y,dx,dy):
+        santa_num = graph[x][y]
+        graph[x][y]=0
+        score[santa_num]+=C
+        shock_lst[santa_num]=-2
+        # 다음 좌표
+        nx,ny = x+C*dx, y+C*dy
+        if nx<1 or nx>N or ny<1 or ny>N:
+            out_lst.append(santa_num)
+            santa_lst[santa_num]=(-1,-1)
+            return
+        if graph[nx][ny] !=0:
+            interaction(graph[nx][ny],dx,dy)
+        santa_lst[santa_num] = [nx,ny]
+        graph[nx][ny]=santa_num
+        
+    def move_rudolph():
+        nonlocal rudolph_pos
+        ru_x, ru_y = rudolph_pos[0][0], rudolph_pos[0][1]
+        near_santa=[]
+        # 가장 가까운 산타에게 돌진
+        for x in range(1,N+1):
+            for y in range(1,N+1):
+                if graph[x][y]==0:
+                    continue
+                near_santa.append((x,y,(ru_x-x)**2+(ru_y-y)**2))
+        sa_x, sa_y, _ = sorted(near_santa, key = lambda x: (x[2],-x[0],-x[1]))[0]
+        if sa_x<ru_x:
+            dx=-1
+        elif sa_x>ru_x:
+            dx=1
+        else:
+            dx=0
+        if sa_y<ru_y:
+            dy=-1
+        elif sa_y>ru_y:
+            dy=1
+        else:
+            dy=0
+        ru_x, ru_y = ru_x+dx, ru_y+dy
+        rudolph_pos=[[ru_x,ru_y]]
+        # 도착 지점에 산타가 있을 때
+        if ru_x == sa_x and ru_y==sa_y:
+            crash_rudolph(sa_x,sa_y,dx,dy)
+            
+    def crash_santa(x,y,dx,dy):
+        santa_num = graph[x-dx][y-dy]
+        graph[x-dx][y-dy]=0
+        score[santa_num]+=D
+        shock_lst[santa_num]=-2
+        # 다음 좌표
+        nx,ny = x-D*dx, y-D*dy
+        if nx<1 or nx>N or ny<1 or ny>N:
+            out_lst.append(santa_num)
+            santa_lst[santa_num]=(-1,-1)
+            return
+        if graph[nx][ny] !=0:
+            interaction(graph[nx][ny],-dx,-dy)
+        santa_lst[santa_num] = [nx,ny]
+        graph[nx][ny]=santa_num
+        
+    def move_santa(santa_num):
+        nonlocal rudolph_pos
+        ru_x, ru_y = rudolph_pos[0][0], rudolph_pos[0][1]
+        x,y = santa_lst[santa_num][0], santa_lst[santa_num][1]
+        dis = (ru_x-x)**2+(ru_y-y)**2
+        santa_dir = []
+        for idx in range(4):
+            nx, ny = x+ santa_direction[idx][0],y+santa_direction[idx][1]
+            # 범위를 넘어가면
+            if nx<1 or nx>N or ny<1 or ny>N:
+                continue
+            # 다른 산타가 있다면
+            if graph[nx][ny] !=0:
+                continue
+            if dis > (ru_x-nx)**2+(ru_y-ny)**2:
+                santa_dir.append(((ru_x-nx)**2+(ru_y-ny)**2,idx))
+        if len(santa_dir)==0:
+            return
+        d = sorted(santa_dir, key = lambda x:(x[0],x[1]))[0][1]
+        dx, dy = santa_direction[d][0],santa_direction[d][1]
+        nx, ny = x+dx, y+dy
+        if nx==ru_x and ny == ru_y:
+            #박치기
+            crash_santa(nx,ny, dx,dy)
+        else:
+            graph[x][y]=0
+            graph[nx][ny] = santa_num
+            santa_lst[santa_num]=[nx,ny]
+        
+    global N, M, P, C, D
+    N,M,P,C,D = map(int, input().split())
+    # 전역변수로 설정
+    rudolph_pos = []
+    ru_x, ru_y = map(int, input().split())
+    rudolph_pos.append([ru_x,ru_y])
+    graph = [[0]*(N+1) for _ in range(N+1)]
+    santa_lst = [[] for _ in range(P+1)]
+    score = [0 for _ in range(P+1)]
+    out_lst = []
+    shock_lst = [0 for _ in range(P+1)]
+    for _ in range(P):
+        num, x, y = map(int,input().split())
+        graph[x][y]=num
+        santa_lst[num] = [x,y] 
+        
+    for m in range(M):    
+        if len(out_lst)==P:
+            break
+        move_rudolph()
+        for i in range(1,P+1):
+            if i in out_lst:
+                continue
+            if shock_lst[i]<0:
+                continue
+            move_santa(i)
+
+        for i in range(1,P+1):
+            if i in out_lst:
+                continue
+            shock_lst[i]+=1
+            score[i]+=1
+    # 출력
+    for i in range(1, P+1):
+        print(score[i], end=' ')
+
+if __name__=="__main__":
+    main()
