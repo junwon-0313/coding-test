bd8525cc475f377dd3aac5d98c3d7a0f70ed54d8
junwon <jjuny9798@gmail.com>
2023-12-27 21:30:50 +0900

    solved: 게리맨더링2

--- 삼성 SW 역량 테스트 기출 문제/백준/게리맨더링2.py
+++ 삼성 SW 역량 테스트 기출 문제/백준/게리맨더링2.py
@@ -1,3 +1,4 @@
+# 행렬 좌표 잘 설정하기 r,c, x,y
 def main():
     N = int(input())
     graph = []
@@ -12,56 +13,47 @@ def main():
     # 시작점
     for x in range(N):
         for y in range(N):
-            # d1, d2 경계a
-            for d1 in range(1,N):
-                for d2 in range(1,N):
+            # d1, d2 경계
+            for d1 in range(1, N):
+                for d2 in range(1, N):
                     # 범위를 벗어날 경우 종료
                     # 꼭짓점이 범위를 나가는 지 체크
-                    if y-d1<0 or x+d1+d2>=N or y+d2>=N:
+                    if x - d1 < 0 or y + d1 + d2 >= N or x + d2 >= N:
                         continue
                     # 5번 구역 체크
-                    visited = [[False]*N for _ in range(N)]
-                    nx, ny = x,y
-                    for j in range(d2+1):
-                        for i in range(d1+1):
-                            visited[ny-i][nx+i]=True
-                        nx, ny = nx+1, ny+1 
-                    nx, ny = x+1,y
+                    visited = [[False] * N for _ in range(N)]
+                    # 시작점에서부터 평행사변형
+                    sx, sy = x, y
+                    for j in range(d2 + 1):
+                        for i in range(d1 + 1):
+                            visited[sx - i][sy + i] = True
+                        sx, sy = sx + 1, sy + 1
+                    sx, sy = x, y + 1
                     for j in range(d2):
                         for i in range(d1):
-                            visited[ny-i][nx+i]=True
-                        nx, ny = nx+1, ny+1
-                    # print('@@@')
-                    # print(x,y,d1,d2)
-                    # for i in range(N):
-                    #     for j in range(N):
-                    #         print(visited[i][j], end = ' ')
-                    #     print()
+                            visited[sx - i][sy + i] = True
+                        sx, sy = sx + 1, sy + 1
                     # x,y,d1,d2가 정해짐 -> 전체 돌면서 조건 탐색
                     # 5번 구역
-                    area = [0]*5
+                    area = [0] * 5
                     for r in range(N):
                         for c in range(N):
-                            
                             if visited[r][c]:
+                                area[4] += graph[r][c]
                                 continue
-                            if 0<=r<x+d1-1 and 0<=c<y:
-                                area[0]+=graph[r][c]
-                            elif 0<= r<x+d2 and y<=c<N:
-                                area[1]+=graph[r][c]
-                            elif x+d1-1<=r<N and 0<=c<y-d1+d2-1:
-                                area[2]+=graph[r][c]
-                            elif x+d2<= r<N and y-d1+d2-1<=c<N:
-                                area[3]+=graph[r][c]
-                    area[4] = total_num - sum(area)
+                            if 0 <= r < x and 0 <= c <= y + d1:
+                                area[0] += graph[r][c]
+                            elif 0 <= r <= x - d1 + d2 and y + d1 < c <= N - 1:
+                                area[1] += graph[r][c]
+                            elif x <= r <= N - 1 and 0 <= c < y + d2:
+                                area[2] += graph[r][c]
+                            elif x + d2 - d1 < r <= N - 1 and y + d2 <= c <= N - 1:
+                                area[3] += graph[r][c]
                     # 최솟값만 유지
-                    print(area, sum(area))
                     local_min = max(area) - min(area)
-                    print(local_min)
                     if local_min < total_min:
-                        total_min = local_min  
-                        
-    print(total_min)  
+                        total_min = local_min
+    print(total_min)
 
-if __name__=='__main__':
+if __name__ == "__main__":
     main()
