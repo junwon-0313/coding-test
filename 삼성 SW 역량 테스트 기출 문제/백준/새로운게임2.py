6d4b9857ae86097370b446641f22915366abf7bf
junwon <jjuny9798@gmail.com>
2024-01-08 18:08:38 +0900

    solving: 새로운 게임2.py
    inital setting

--- 삼성 SW 역량 테스트 기출 문제/백준/새로운게임2.py
+++ 삼성 SW 역량 테스트 기출 문제/백준/새로운게임2.py
@@ -1,4 +1,26 @@
+# 체스판과 말 상태는 어떻게 관리할 것인가?
+# 말을 순서대로 관리해야함. 위에서부터 왼쪽 
 def main():
-    pass
+    N, K = map(int,input().split())
+    # 체스판
+    graph = []
+    for _ in range(N):
+        graph.append(list(map(int,input().split())))
+    horse = [[[] for _ in range(N)] for _ in range(N)]
+    for idx in range(K):
+        row, col, dir = map(int,input().split())
+        row-=1
+        col-=1
+        horse[row][col].append((idx+1,dir))
+    
+    # 출력
+    def print_horse_pos():
+        print('@@@@@@@@@@@@@@')
+        for x in range(N):
+            for y in range(N):
+                print(horse[x][y], end =' ')
+            print()
+    print_horse_pos()
+    
 if __name__ == "__main__":
     main()
